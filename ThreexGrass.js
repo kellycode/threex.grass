4fd393a9820854c361dd3056a391e8d43eb04524
KellyCode <kearnankelly@gmail.com>
2025-03-14 08:07:05 -0500

    color adjustments

--- ThreexGrass.js
+++ ThreexGrass.js
@@ -2,17 +2,17 @@ import * as THREE from "three";
 
 export class ThreexGrass {
     static createGrassTufts(scene, positions, texture, size) {
-        // create the initial geometry
+
+        // the initial geometry
         let geometry = new THREE.PlaneGeometry(size.w, size.h);
+        // to merge the geometries for performance
+        let mergedGeo = new THREE.Object3D();
 
         let material = new THREE.MeshPhongMaterial({
             map: texture,
             alphaTest: 0.7,
         });
 
-        // create each tuft and merge their geometry for performance
-        //let mergedGeo = new THREE.Geometry();
-        let mergedGeo = new THREE.Object3D();
 
         for (let i = 0; i < positions.length; i++) {
             let position = positions[i];
@@ -24,10 +24,10 @@ export class ThreexGrass {
                 let angle = baseAngle + (j * Math.PI) / nPlanes;
 
                 // First plane
-                let object3d_f = new THREE.Mesh(geometry, material);
-                object3d_f.rotateY(angle);
-                object3d_f.position.copy(position);
-                mergedGeo.add(object3d_f);
+                let object3d_a = new THREE.Mesh(geometry, material);
+                object3d_a.rotateY(angle);
+                object3d_a.position.copy(position);
+                mergedGeo.add(object3d_a);
 
                 // The other side of the plane
                 let object3d_b = new THREE.Mesh(geometry, material);
